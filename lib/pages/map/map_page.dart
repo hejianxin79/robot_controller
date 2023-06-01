b87f40dc64935caa86d4ff280b0add46bcf610a0
hejianxin79 <hejianxin79@hotmail.com>
2023-06-01 19:58:42 +0800

    在地图标记坐标
    在地图标记坐标，并且判断坐标之间过近则不添加

--- lib/pages/map/map_page.dart
+++ lib/pages/map/map_page.dart
@@ -14,11 +14,26 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  //点击地图后记录点击位置在地图上的像素位置
   Offset localPosition = const Offset(-1, -1);
+  //点击地图后获取像素颜色
   Color color = const Color(0x00000000);
+  //获取当前点击像素颜色
   Color c = Color(0x000000);
+  //获取当前点击地图像素位置
   Offset p = const Offset(-1, -1);
   bool is_show_msg = true;
+  //坐标视图位置列表。
+  List<Widget> markList = [
+    const Image(
+      width: 384,
+      height: 384,
+      //fit: BoxFit.contain,
+      image: AssetImage("images/map.jpg"),
+    ),
+  ];
+  //坐标位置列表
+  List<Offset> offsetList = [];
   Future<void> socketUtil(BuildContext context,String host, int port,String params) async {
     try {
       var socket = await Socket.connect(host, port);
@@ -47,11 +62,13 @@ class _MapPageState extends State<MapPage> {
     );
   }
   void _getTapPosition(BuildContext context) async{
+    //判断是否点击了同一个位置。
     if(this.c != this.color){
       setState(() {
         this.c = this.color;
       });
     }
+    //判断是否点击了同一个位置。
     if(this.p != this.localPosition){
       setState(() {
         this.p = this.localPosition;
@@ -68,6 +85,31 @@ class _MapPageState extends State<MapPage> {
     if(this.color == Color(0xffdcdcdc) || this.color == Color(0xffcdcdcd)){
       Toast.toast(context,msg: "所选位置机器人可能无法到达",showTime: 1500);
     }else{
+      int count = markList.length + 1;
+      setState(() {
+        markList.add(
+          Positioned(
+            left: localPosition.dx - 10,
+            top: localPosition.dy - 10,
+            child: InkWell(
+              onTap: (){
+                print(count.toString());
+              },
+              child: Container(
+                width: 30,
+                height: 30,
+                color: Colors.blue,
+                child: const Image(
+                  width: 20,
+                  height: 20,
+                  image: AssetImage("images/mark.png"),
+                ),
+              ),
+            ),
+          ),
+        );
+        offsetList.add(localPosition);
+      });
       _showBasicModalBottomSheet(context,par);
     }
 
@@ -95,37 +137,33 @@ class _MapPageState extends State<MapPage> {
                 );
                 WidgetsBinding.instance!.addPostFrameCallback((_) {
                   if (mounted) {
-
-                    setState(() {
-                      if (localPosition != this.p){
-
+                    //判断坐标列表里是否存在点击的坐标范围
+                    //bool is_offset = _offsetRange(localPosition);
+                    if(offsetList.length==0){
+                      setState(() {
                         this.color = color;
                         _getTapPosition(context);
+                      });
+                    }else {
+                      if (_offsetRange(context,localPosition) == false) {
+                        //判断本次点击是否在上次点击的安全范围之外x
+                        int a = localPosition.dx.toInt() - this.p.dx.toInt();
+                        //判断本次点击是否在上次点击的安全范围之外y
+                        int b = localPosition.dy.toInt() - this.p.dy.toInt();
+                        if (localPosition != this.p) {
+                          if ((a <= -20 || a >= 20) || (b <= -20 || b >= 20)) {
+                            setState(() {
+                              this.color = color;
+                              _getTapPosition(context);
+                            });
+                          }
+                        }
                       }
-                      //print(color);
-
-                    });
-
+                    }
                   }
                 });
                 return Stack(
-                  children: [
-                    const Image(
-                      width: 384,
-                      height: 384,
-                      //fit: BoxFit.contain,
-                      image: AssetImage("images/map.jpg"),
-                    ),
-                    Positioned(
-                      left: localPosition.dx - 10,
-                      top: localPosition.dy - 10,
-                      child: const Image(
-                        width: 20,
-                        height: 20,
-                        image: AssetImage("images/direction.png"),
-                      ),
-                    ),
-                  ],
+                  children: markList,
                 );
               }
           ),
@@ -134,6 +172,32 @@ class _MapPageState extends State<MapPage> {
     );
 
   }
+  /*
+  //循环当前点为列表，判断当前点击的点为坐标是否与列表内坐标距离过近
+   */
+  bool _offsetRange(BuildContext context, Offset loc){
+    //记录与当前点击的坐标过近的数量
+    int ab = 0;
+    for(Offset of in offsetList){
+      //判断本次点击是否在上次点击的安全范围之外x
+      int a = loc.dx.toInt() - of.dx.toInt();
+      //判断本次点击是否在上次点击的安全范围之外y
+      int b = loc.dy.toInt() - of.dy.toInt();
+      //范围为+-20像素以上
+      if((a<=-20 || a>=20) || (b<=-20 || b>=20)){
+
+      }else{
+        ab += 1;
+      }
+    }
+    //如果列表内不存在过近点为则返回false
+    if(ab == 0){
+      return false;
+    }
+    return true;
+
+  }
+
   void showBottomSheet(BuildContext context) {
     //用于在底部打开弹框的效果
     showModalBottomSheet(
@@ -164,8 +228,8 @@ class _MapPageState extends State<MapPage> {
             ),
             Container(
               width: MediaQuery.of(context).size.width,
-              padding: EdgeInsets.only(left: 10),
-              child: Text("去邀请"),
+              padding: const EdgeInsets.only(left: 10),
+              child: const Text("去邀请"),
             ),
 
           ],
@@ -176,17 +240,18 @@ class _MapPageState extends State<MapPage> {
       isScrollControlled: false,
       context: context,
       builder: (BuildContext context) {
-        return ListView.builder(
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-                title: Text(options[index]),
-                onTap: () {
-                  Navigator.of(context).pop(index);
-                });
-          },
-          itemCount: options.length,
+        return Container(
+          height: 300.h,
+          color: Colors.red,
+          child: const Column(
+            children: [
+              //TextField(),
+            ],
+          ),
         );
       },
     );
   }
+
+
 }
